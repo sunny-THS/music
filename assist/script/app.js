cff1653383cc47171f6ce338d919ab857d18f0f1
sunny-THS <tuhueson@gmail.com>
2021-02-17 10:46:39 +0700

    sd

---
+++ assist/script/app.js
@@ -0,0 +1,47 @@
+document.querySelector('.submit_').addEventListener('click', function(e) {
+  const inputFile = document.querySelector('.inputfile');
+  const files = inputFile.files;
+  let folder =  prompt('Xin mời nhập tên','');
+  uploadFile(files, folder);
+});
+function uploadFile(files, folder) {
+  var titlePage = document.querySelector('title');
+  const date = new Date(Date.now()); // get datetime now
+
+  Array.prototype.forEach.call(files, function(file) {
+    const nameFile = file.name;
+    const Itemfile = file;
+    const storageRef = firebase.storage().ref();
+    const filesRef = storageRef.child(`music/${folder}/${nameFile}`).put(Itemfile);
+
+    filesRef.on('state_changed', snapshot => {
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      titlePage.textContent = `Upload is ${progress}% done`;
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          titlePage.textContent = 'Upload is paused';
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, error => {
+      // Handle unsuccessful uploads
+      alert(error);
+    }, () => {
+      // Handle successful uploads on complete
+      titlePage.textContent = 'Save music';
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      // filesRef.snapshot.ref.getDownloadURL().then((downloadURL) => {
+      //   const setupFile = {
+      //     name: file.name,
+      //     type: file.type == '' ? 'application/octet-stream' : file.type,
+      //     date: date.toLocaleString('en-GB'),
+      //     folder: localStorage.username == '' ? 'root' : localStorage.username,
+      //     url_data: downloadURL
+      //   }
+      //   socket.emit('ClientSendData', setupFile);
+      // });
+    });
+  });
+}
---
+++ assist/script/initFirebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA9DfX5O8YbJSvU0Yy0YnG9RDgooEuomOc",
+    authDomain: "music-app100.firebaseapp.com",
+    projectId: "music-app100",
+    storageBucket: "music-app100.appspot.com",
+    messagingSenderId: "700799537454",
+    appId: "1:700799537454:web:830ecad5cc21f5bb20eabf",
+    measurementId: "G-LT946S5PJ3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  document.addEventListener("DOMContentLoaded", event => {
+    var app = firebase.app();
+  });
