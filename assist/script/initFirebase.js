d331f641f34c38d0063a228fb62afe9e0c994d62
sunny-THS <tuhueson@gmail.com>
2021-02-24 12:58:10 +0700

    sd

--- assist/script/initFirebase.js
+++ assist/script/initFirebase.js
@@ -7,9 +7,9 @@ var firebaseConfig = {
     appId: "1:700799537454:web:830ecad5cc21f5bb20eabf",
     measurementId: "G-LT946S5PJ3"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  document.addEventListener("DOMContentLoaded", event => {
-    var app = firebase.app();
-  });
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+document.addEventListener("DOMContentLoaded", event => {
+  var app = firebase.app();
+});
